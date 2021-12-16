5d7a9951e9ff1dcf090244edff31b7b68e184805
brylimz <superuser@brylimz.com>
2021-12-16 19:44:32 +0700

    wp

--- day27.py
+++ day27.py
@@ -1,11 +1,29 @@
-import tkinter
-window = tkinter.Tk()
+from tkinter import *
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # label
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+my_label.config(text="new text")
+
+# Button
+
+
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text=input.get())
+
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+# Entry
+input = Entry(width=10)
+input.pack()
+input.get()
+
 
 
 
--- playground.py
+++ playground.py
@@ -1,11 +1,41 @@
 import random
 
 
-def add(*args):
-    sum = 0
-    for num in args:
-        sum = sum+num
-    print(sum)
+#
+# def foo(a, b=4, c=6):
+#     print(a, b, c)
+#
+#
+# foo(20, c=7)
+
+#
+# def add(*args):
+#     sum = 0
+#     for n in args:
+#         sum += n
+#     return sum
+#
+#
+# print(add(add(1, 10)))
+#
+#
+# def calculate(**kwargs):
+#     print(kwargs)
+#     print([kwargs])
+#
+#
+# calculate(add=3, multiply=5)
+#
+#
+# class Car:
+#     def __init__(self, **kw):
+#         self.make = kw.get("make")
+#         self.model = kw["model"]
+#
+
+# quiz
+def all_aboard(a, *args, **kwargs):
+    print(a, args, kwargs)
 
 
-add(random.randint(1, 10))
+all_aboard(4, 7, 3, 0, x=10, y=64)
